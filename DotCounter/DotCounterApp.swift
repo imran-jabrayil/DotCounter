a180c7ece8493182f528d19751d081002c8e830a
Imran Jabrayilov <c.imran2001@yandex.com>
2025-06-22 11:35:09 +0400

    Initial Commit

---
+++ DotCounter/DotCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotCounterApp.swift
+//  DotCounter
+//
+//  Created by Imran Jabrayilov on 22.06.25.
+//
+
+import SwiftUI
+
+@main
+struct DotCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
